b47e4b5dc8e7c71b7d120caf37d2ae4df9bdde3b
Evgeny Lyozin <evgenylyozindev@gmail.com>
2022-08-11 12:18:56 +0300

    Переписал модуль под работу со стримами процесса

--- io/processSTDIO.ts
+++ io/processSTDIO.ts
@@ -1,3 +1,10 @@
+console.log(
+  'Эхо сервер, напишите что-то, нажмите enter и сервер ответит той же строкой:\n'
+)
 process.stdin.on('data', (data) => {
-  console.log(data.toString())
+  if (data.toString().length < 2) {
+    process.stderr.write('Ничего не написали\n')
+  } else {
+    process.stdout.write(data.toString())
+  }
 })
