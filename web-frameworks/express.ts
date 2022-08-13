25b94489a65542f1eed0fa4814edd42884a6208d
Evgeny Lyozin <evgenylyozindev@gmail.com>
2022-08-13 10:13:11 +0300

    сервер на основе экспресса

---
+++ web-frameworks/express.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import path from 'path'
+import indexRouter from './expressIndexRouter'
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.static(path.resolve(process.cwd(), '/web-frameworks/public')))
+
+app.use('/', indexRouter)
+
+app.listen(PORT, () => {
+  console.log('Экспресс сервер готов и слушает на порту ', PORT)
+})
