75e30559c534b2a8b0104a083a223114fd0964cc
Evgeny Lyozin <evgenylyozindev@gmail.com>
2022-08-13 14:19:38 +0300

    Добавил пример миддлвары экспресса

--- web-frameworks/express.ts
+++ web-frameworks/express.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import indexRouter from './expressIndexRouter'
+import logger from './expressMiddleware'
 
 const PORT = process.env.PORT || 3000
 
@@ -9,6 +10,9 @@ const app = express()
 app.set('views', path.resolve(process.cwd(), 'web-frameworks/views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.resolve(process.cwd(), 'web-frameworks/public')))
+
+app.use(logger)
+
 app.use('/', indexRouter)
 
 app.listen(PORT, () => {
